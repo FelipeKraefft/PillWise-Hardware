c2af913e00e98d1581d66c655a65c54ed18694d7
Felipe Kraefft <101569052+FelipeKraefft@users.noreply.github.com>
2023-06-28 10:00:11 -0300

    Prueba Mario

---
+++ Prueba Mario/Mario/Mario.ino
@@ -0,0 +1,38 @@
+
+#define ledPin 2
+#define buzzerPin 9
+
+void setup() {
+  // Set pin 2 as an output.
+  pinMode(ledPin, OUTPUT);
+
+  // Initialize the serial port.
+  Serial.begin(9600);
+}
+
+void loop() {
+  // If the serial port receives the string "prender", turn on pin 2.
+  while (Serial.available() == 0) {} 
+  String line = Serial.readString();
+  line.trim();    
+  
+  if (line == "prender") {
+    digitalWrite(ledPin, HIGH);
+  } 
+  
+  else if (line == "apagar") {
+    digitalWrite(ledPin, LOW);
+  }
+
+  else if (line == "buzzer"){
+    sing(1, buzzerPin);
+    sing(2, buzzerPin);
+    sing(3, buzzerPin);
+  }
+  else
+  {
+    Serial.print(line);
+  }
+
+  
+}
